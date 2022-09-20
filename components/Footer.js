d8563518de45db9e3062dc12b6aade6b27e3d73c
Lakshmikanth AM <chetan16061993@gmail.com>
2022-09-20 17:27:32 +0530

    updating header and footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,5 @@
 import { Footer } from 'flowbite-react';
-import { BsFacebook, BsInstagram, BsTwitter } from './svgs/SocialMedia';
+import { BsFacebook, BsInstagram, BsWhatsApp } from './svgs/SocialMedia';
 
 function CommonFooter() {
   return (
@@ -16,24 +16,30 @@ function CommonFooter() {
             </div>
             <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6">
               <div>
-                <Footer.Title title="about" />
+                <Footer.Title title="Site Links" />
                 <Footer.LinkGroup col={true}>
-                  <Footer.Link href="#">Vruksha Natyalaya</Footer.Link>
-                  <Footer.Link href="#">Some Link</Footer.Link>
+                  <Footer.Link href="/">Home</Footer.Link>
+                  <Footer.Link href="/background">Background</Footer.Link>
+                  <Footer.Link href="/about">About</Footer.Link>
+                  <Footer.Link href="/gallery">Gallery</Footer.Link>
+                  <Footer.Link href="/contact">Contact</Footer.Link>
                 </Footer.LinkGroup>
               </div>
               <div>
                 <Footer.Title title="Follow us" />
                 <Footer.LinkGroup col={true}>
-                  <Footer.Link href="#">Github</Footer.Link>
+                  <Footer.Link href="https://www.instagram.com/vruksha__natyalaya/?igshid=YmMyMTA2M2Y%3D">
+                    Instagram
+                  </Footer.Link>
                   <Footer.Link href="#">Facebook</Footer.Link>
                 </Footer.LinkGroup>
               </div>
               <div>
-                <Footer.Title title="Legal" />
+                <Footer.Title title="Contact us" />
                 <Footer.LinkGroup col={true}>
-                  <Footer.Link href="#">Privacy Policy</Footer.Link>
-                  <Footer.Link href="#">Terms & Conditions</Footer.Link>
+                  <Footer.Link href="https://wa.me/9845396648">
+                    Whatsapp
+                  </Footer.Link>
                 </Footer.LinkGroup>
               </div>
             </div>
@@ -43,8 +49,11 @@ function CommonFooter() {
             <Footer.Copyright href="#" by="Vruksha Natyalaya" year={2022} />
             <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
               <Footer.Icon href="#" icon={BsFacebook} />
-              <Footer.Icon href="#" icon={BsInstagram} />
-              <Footer.Icon href="#" icon={BsTwitter} />
+              <Footer.Icon
+                href="https://www.instagram.com/vruksha__natyalaya/?igshid=YmMyMTA2M2Y%3D"
+                icon={BsInstagram}
+              />
+              <Footer.Icon href="https://wa.me/9845396648" icon={BsWhatsApp} />
             </div>
           </div>
         </div>
