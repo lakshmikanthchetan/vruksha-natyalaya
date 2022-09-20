f3bec6fcd10dad72d21dad00f53823c107f66271
Lakshmikanth AM <chetan16061993@gmail.com>
2022-09-20 16:57:50 +0530

    updating head tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,24 @@
+import Head from 'next/head';
 import '../styles/globals.css';
 
 // To check css output file of globals, use npm run build:css
 // import '../styles/globals-output.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Vruksha Natyalaya</title>
+        <link
+          rel="shortcut icon"
+          href="/images/dark-logo.jpeg"
+          type="image/x-icon"
+        />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
