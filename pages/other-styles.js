17869155db5595abe87f737522d682e7a94d2e89
Lakshmikanth AM <chetan16061993@gmail.com>
2022-09-22 16:42:31 +0530

    other styles

---
+++ pages/other-styles.js
@@ -0,0 +1,101 @@
+import Image from 'next/image';
+import React from 'react';
+
+const cardsData = [
+  {
+    name: 'Dance Fitness - Zumba',
+    url: '/images/zumba.jpg',
+    title: 'Dance Fitness- Zumba',
+    desc: 'Zumba is a fitness program that involves cardio and Latin-inspired dance. It was founded by Colombian dancer and choreographer Beto Pérez in 2001 and by 2012, it had 110,000 locations and 12 million people taking classes weekly. Zumba is a trademark owned by Zumba Fitness, LLC.',
+    readMoreUrl: 'https://en.wikipedia.org/wiki/Zumba',
+  },
+  {
+    name: 'Bollywood',
+    url: '/images/bollywood.jpeg',
+    title: 'Bollywood',
+    desc: 'Hindi dance music encompasses a wide range of songs predominantly featured in the Bollywood film industry with a growing worldwide attraction. The music became popular among overseas Indians in places such as South Africa, Mauritius, Fiji, the Caribbean, Canada, the United Kingdom, the Netherlands and the United States of America and eventually developed a global fan base.',
+    readMoreUrl: 'https://en.wikipedia.org/wiki/Hindi_dance_music',
+  },
+  {
+    name: 'Kathak',
+    url: '/images/kathak.jpeg',
+    title: 'Kathak',
+    desc: 'Kathak is one of the eight major forms of Indian classical dance. It is the classical dance form of Uttar Pradesh. The origin of Kathak is traditionally attributed to the traveling bards in ancient northern India known as Kathakars or storytellers.',
+    readMoreUrl: 'https://en.wikipedia.org/wiki/Kathak',
+  },
+  {
+    name: 'Kuchipudi',
+    url: '/images/kuchipudi.webp',
+    title: 'Kuchipudi',
+    desc: 'Kuchipudi is a dance-drama performance, with it’s roots in the ancient Hindu Sanskrit text of Natya Shastra. It developed as a religious art linked to traveling bards, temples and spiritual beliefs, like all major classical dances of India.',
+    readMoreUrl: 'https://en.wikipedia.org/wiki/Kuchipudi',
+  },
+  {
+    name: 'Yoga',
+    url: '/images/yoga.jpg',
+    title: 'Yoga',
+    desc: 'Yoga is a group of physical, mental, and spiritual practices or disciplines which originated in ancient India and aim to control (yoke) and still the mind, recognizing a detached witness-consciousness untouched by the mind (Chitta) and mundane suffering (Duḥkha). There is a wide variety of schools of yoga, practices, and goals in Hinduism, Buddhism, and Jainism, and traditional and modern yoga is practiced worldwide.',
+    readMoreUrl: 'https://en.wikipedia.org/wiki/Yoga',
+  },
+];
+
+const Card = (props) => {
+  const { name = '', url = '#', title, desc, readMoreUrl } = props;
+  return (
+    <div className="mx-auto text-center max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <a href="#">
+        <Image
+          className="rounded-t-lg"
+          src={url}
+          alt={name}
+          width={400}
+          height={300}
+        />
+      </a>
+      <div className="p-5">
+        <a href="#">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {title}
+          </h5>
+        </a>
+        <p className="mb-5 font-normal text-gray-700 dark:text-gray-400">
+          {desc}
+        </p>
+        {readMoreUrl && (
+          <a
+            href={readMoreUrl}
+            className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Read more
+            <svg
+              aria-hidden="true"
+              className="ml-2 -mr-1 w-4 h-4"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </a>
+        )}
+      </div>
+    </div>
+  );
+};
+function otherStyles() {
+  return (
+    <div className="container mx-auto">
+      <section className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+        {cardsData.map((card) => (
+          <Card key={card.url} {...card} />
+        ))}
+      </section>
+    </div>
+  );
+}
+
+export default otherStyles;
